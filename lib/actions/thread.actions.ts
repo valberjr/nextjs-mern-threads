3f3c370b2ed91b63393250528e4c56435d5db70d
valberjr <3cifhzht@duck.com>
2023-08-31 16:11:35 -0300

    fetch posts homepage and thread card structure

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -39,3 +39,81 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch the posts that have no parents (top-level threads...)
+  const postQuery = Thread.find({
+    parentId: {
+      $in: [null, undefined],
+    },
+  })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: 'author',
+      model: User,
+    })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image',
+      },
+    });
+
+  const totalPostCount = await Thread.countDocuments({
+    parentId: {
+      $in: [null, undefined],
+    },
+  });
+
+  const posts = await postQuery.exec();
+
+  const isNext = totalPostCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+  // TODO: populate community
+
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name parentId image',
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name parentId image',
+            },
+          },
+        ],
+      })
+      .exec();
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
+  }
+}
