a718166efcd4b869a2af12099cf6700de597fa3c
valberjr <vjunior@skiff.com>
2023-09-18 14:10:27 -0300

    activity page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -138,3 +138,30 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users: ${error.message}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // collect all the child threads ids (replies) from the 'children field
+    const childrenThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childrenThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image id',
+    });
+
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message}`);
+  }
+}
